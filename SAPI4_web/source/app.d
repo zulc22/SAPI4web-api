0b950276f5b5018b16bf20cd0427ad7e825acfae
Sadie <zulc22db+oss@gmail.com>
2026-02-20 11:14:10 -0800

    Fix regression in /VoiceList
    Didn't test this endpoint before commiting. `SortedRange` objects don't seem to work correctly with `HTTPServerResponse.writeJsonBody`?

--- SAPI4_web/source/app.d
+++ SAPI4_web/source/app.d
@@ -81,7 +81,7 @@ class SAMService
 	@path("/VoiceList")
 	void getVoiceList(HTTPServerRequest req, HTTPServerResponse res)
 	{
-		res.writeJsonBody(SAMS.keys.sort);
+		res.writeJsonBody(SAMS.keys);
 	}
 
 	@path("/VoiceLimitations")
